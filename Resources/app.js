161d6fa66a6d01a7c14594230dea6190b4713e5f
Keenan DuBois <keenan.m.dubois@gmail.com>
2017-03-10 14:49:56 -0500

    Update app.js

--- Resources/app.js
+++ Resources/app.js
@@ -2,12 +2,6 @@
  * A tabbed application, consisting of multiple stacks of windows associated with tabs in a tab group.
  * A starting point for tab-based application with multiple top-level windows.
  * Requires Titanium Mobile SDK 1.8.0+.
- *
- * In app.js, we generally take care of a few things:
- * - Bootstrap the application with any data we need
- * - Check for dependencies like device type, platform version or network connection
- * - Require and open our top-level UI component
- *
  */
 
 //bootstrap and check dependencies
